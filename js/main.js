ec7a211b7ee06e46fb2adabd8cb584246f699dc1
SLIYAZ1 <liyaz.shaik160120@gmail.com>
2026-01-09 19:57:47 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
-const music = document.getElementById("bgMusic");
+const text =
+  "From the moment you entered my life, everything became softer, calmer, and more beautiful.";
+let i = 0;
+
+function type() {
+  if (i < text.length) {
+    document.getElementById("type").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 60);
+  }
+}
 
 function unlock() {
   const pwd = document.getElementById("password").value;
   if (pwd === "Ayisha") {
     document.getElementById("lockScreen").style.display = "none";
     document.getElementById("mainContent").classList.remove("hidden");
-    music.play();
+    document.getElementById("bgMusic").play();
+    type();
   } else {
-    alert("Wrong password 💔");
+    alert("Not yet 💔");
   }
+}
+
+function goYes() {
+  window.location.href = "yes.html";
 }
